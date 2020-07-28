b371081645574424b83107f90994c7192dda9c4f
yongyesun <68177406+yongyesun@users.noreply.github.com>
2020-07-28 15:36:47 +0800

    Update diy-part2.sh
    try fix!

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,4 +10,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/coolsnowwolf/lede/package/lean
+git clone https://github.com/coolsnowwolf/lede/tree/master/package/lean
