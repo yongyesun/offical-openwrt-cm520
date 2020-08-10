37e22a0fe87f4e4d04b4d17a478c60dcca0339c1
yongyesun <68177406+yongyesun@users.noreply.github.com>
2020-08-10 13:58:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/yongyesun/luci-app-koolproxy package/luci-app-koolproxy
-git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
+git clone --depth 1 https://github.com/yongyesun/luci-app-koolproxy package/luci-app-koolproxy
+git clone --depth 1 https://github.com/kenzok8/openwrt-packages package/kenzok8
+git clone --depth 1 https://github.com/yongyesun/lede package/lede
+cp -r package/lede/package/lean package/lean
+rm -rf package/lede
