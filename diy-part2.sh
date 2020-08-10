0b4856f7eac751ec96132afcc58f1b3b5001fa6e
yongyesun <68177406+yongyesun@users.noreply.github.com>
2020-08-10 11:32:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,3 +11,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/yongyesun/luci-app-koolproxy package/luci-app-koolproxy
+git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
